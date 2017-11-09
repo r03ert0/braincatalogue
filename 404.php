0350941471f37056d056a5bb76d76fa07abd11e1
r03ert0 <rto@braincatalogue.org>
2017-11-09 15:48:56 +0100

    arrange code

--- 404.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include $_SERVER['DOCUMENT_ROOT']."/php/braincatalogue.php";
-	
-	$uri=$_SERVER['REQUEST_URI'];
-	$args=array_filter(explode("/",$uri));
-	braincatalogue(array_filter(explode("/",parse_url($uri,PHP_URL_PATH))));
-?>
